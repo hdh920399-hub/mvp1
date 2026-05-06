779097f665429d37824db46daf361dfbbc7030a8
hdh920399-hub <hdh920399@gmail.com>
2026-05-06 15:20:35 +0800

    Refactor AdaptiveLearner methods and remove trade history

--- engine/adaptive_learner.py
+++ engine/adaptive_learner.py
@@ -1,15 +1,5 @@
-from collections import deque
-import numpy as np
-
 class AdaptiveLearner:
-    def __init__(self, max_history=100):
-        self.trade_history = deque(maxlen=max_history)
-    
-    def record_trade(self, trade):
-        self.trade_history.append(trade)
-    
     def get_learning_summary(self):
-        return "胜率: 54% | 平均盈亏: +2.3U"
-    
-    def adapt_params(self, current_params):
-        return current_params, "表现良好"
+        return "胜率: 50% | 平均盈亏: 0U"
+    def adapt_params(self, params):
+        return params, "无调整"
